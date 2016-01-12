904def17828fd93d3a4a2fb51fd6ac86bad75fb3
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-01-12 18:06:26 +0800

    kernel.mk: split wl driver rules into another makefile
    Two purposes for this change:
    
    * Centralize the wl related files into the wl subdirectory
    * Can simply ignore wl driver on porting new kernel

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -64,8 +64,8 @@ $(KERNEL_MODULES_DEP): $(INSTALLED_KERNEL_TARGET) $(patsubst %,$(TARGET_OUT_INTE
 	$(hide) rm -rf $(TARGET_OUT)/lib/modules
 	$(mk_kernel) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) modules_install
 	+ $(hide) for kmod in $(TARGET_EXTRA_KERNEL_MODULES) ; do \
-	        echo Installing additional kernel module $${kmod} ; \
-	        $(subst +,,$(subst $(hide),,$(mk_kernel))) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/$${kmod}.kmodule modules_install ; \
+		echo Installing additional kernel module $${kmod} ; \
+		$(subst +,,$(subst $(hide),,$(mk_kernel))) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/$${kmod}.kmodule modules_install ; \
 	done
 	$(hide) rm -f $(TARGET_OUT)/lib/modules/*/{build,source}
 endif
@@ -75,17 +75,7 @@ $(BUILT_SYSTEMIMAGE): $(KERNEL_MODULES_DEP)
 # rules to get source of Broadcom 802.11a/b/g/n hybrid device driver
 # based on broadcomsetup.sh of Kyle Evans
 WL_PATH := $(KERNEL_DIR)/drivers/net/wireless/wl
-WL_ENABLED := $(if $(wildcard $(WL_PATH)),$(shell grep ^CONFIG_WL=[my] $(KERNEL_CONFIG_FILE)))
-WL_ARCH_CHANGED := $(if $(shell file $(WL_PATH)/lib/wlc_hybrid.o_shipped | grep -s $(if $(filter x86,$(TARGET_KERNEL_ARCH)),80386,x86-64)),,FORCE)
-WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_271.tar.gz
-$(WL_SRC):
-	@echo Downloading $(@F)...
-	$(hide) curl http://www.broadcom.com/docs/linux_sta/$(@F) > $@
-$(WL_PATH)/Makefile : $(WL_SRC) $(wildcard $(WL_PATH)/*.patch) $(WL_ARCH_CHANGED) $(KERNEL_ARCH_CHANGED)
-	$(hide) tar zxf $< -C $(@D) --overwrite && \
-		patch -p5 -d $(@D) -i wl.patch && \
-		patch -p1 -d $(@D) -i linux-recent.patch
-$(INSTALLED_KERNEL_TARGET): $(if $(WL_ENABLED),$(WL_PATH)/Makefile)
+-include $(WL_PATH)/build.mk
 
 installclean: FILES += $(KBUILD_OUTPUT) $(INSTALLED_KERNEL_TARGET)
 
